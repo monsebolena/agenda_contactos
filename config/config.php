fb5065c8757fd8e0bab02b34f34aa887cbc8f4c0
monsebolena <monsebaldo7@gmail.com>
2025-02-20 17:13:37 +0100

    estructura

--- config/config.php
+++ config/config.php
@@ -2,5 +2,7 @@
 define("HOST", "localhost");
 define("DB_USER","root");
 define("DB_PASS","");
-define("DB_DATABASE","agenda_contactos");
+define("DB_DATABASE","apptask");
+define('BASE_URL', 'http://localhost/mvcTasks/');
+
 ?>
